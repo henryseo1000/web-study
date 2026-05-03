3b84447ea6783df110b0656e64fe7faf0033e852
서호준 <henryseo1000@gmail.com>
2026-05-04 01:44:04 +0900

    file & firebase study

--- server.py
+++ server.py
@@ -1,59 +1,34 @@
-import os, time, threading, random;
-import smtplib;
-from flask import Flask, render_template, url_for, redirect, request, jsonify, abort;
-from watchdog.observers import Observer;
-from watchdog.events import FileSystemEventHandler;
+import os, random;
+from flask import Flask, render_template, url_for, redirect, request, jsonify, abort, send_file;
 from dotenv import load_dotenv;
 from flask_mail import Message, Mail;
+import firebase_admin;
+from firebase_admin import credentials;
+from firebase_admin import firestore;
+from firebase_admin import db;
 
-from config import *
+from config import *;
 
+# init app
 app = Flask("__name__");
-
 path = "./templates";
 
+# firebase settings
+cred = credentials.Certificate("./mjusubwaystation-firebase-adminsdk-lxlon-53f126911e.json");
+firebase_admin.initialize_app(cred, {
+    'databaseURL': 'https://mjusubwaystation-default-rtdb.firebaseio.com'
+});
+firestore_db = firestore.client()
+
+#load env variables
 load_dotenv()
 
 def redirect_to():
     return url_for('static', filename='favicon/favicon.ico');
 
-class Target:
-    watchDir = os.getcwd()
-    #watchDir에 감시하려는 디렉토리를 명시한다.
+app.add_url_rule('/favicon.ico', 'redirect_to', redirect_to);
 
-    def __init__(self):
-        self.observer = Observer()   #observer객체를 만듦
-
-    def run(self):
-        event_handler = Handler()
-        self.observer.schedule(event_handler, self.watchDir, recursive=True)
-        self.observer.start()
-        try:
-            while True:
-                time.sleep(1)
-        except:
-            self.observer.stop()
-            print("Error")
-            self.observer.join()
-
-
-class Handler(FileSystemEventHandler):
-#FileSystemEventHandler 클래스를 상속받음.
-#아래 핸들러들을 오버라이드 함
-
-    #파일, 디렉터리가 move 되거나 rename 되면 실행
-    def on_moved(self, event):
-        print(event)
-
-    def on_created(self, event): #파일, 디렉터리가 생성되면 실행
-        print(event)
-
-    def on_deleted(self, event): #파일, 디렉터리가 삭제되면 실행
-        print(event)
-
-    def on_modified(self, event): #파일, 디렉터리가 수정되면 실행
-        print(event)
-
+#routes for web
 @app.route("/")
 def index():
     return render_template("index.html");
@@ -62,6 +37,15 @@ def index():
 def route(path):
     try :
         return render_template("/" + path + "/index.html");
+    except:
+        abort(404)
+
+@app.route("/videos/<int:path>")
+def video_route(path):
+    video_title = os.listdir("./static/videos")[path - 1]
+
+    try :
+        return send_file("./static/videos/"+ video_title);
     except:
         abort(404)
 
@@ -106,19 +90,29 @@ def getDir() :
         "totalNum" : len(dir_list)
     };
 
-def watch_file() :
-    file_watcher = Target();
-    file_watcher.run();
+@app.route('/getESP32')
+def getESP32():
+    distance_ref = db.reference('/distance');
+    led_ref = db.reference('/led');
 
+    distances = distance_ref.get();
+    led_stats = led_ref.get()
+
+    return jsonify({"led": led_stats, "distance": distances}), 200
+
+@app.route("/setESP32", methods=['POST'])
+def setESP32():
+    distance_ref = db.reference('/distance');
+    led_ref = db.reference('/led');
+
+    distance_id = distance_ref.push(1);
+    led_id = led_ref.push(False);
+
+    return jsonify({"led_id": led_id.key, "distance_id": distance_id.key}), 201
+
+@app.errorhandler(404)
 def page_not_found(e):
     return render_template("404-notfound.html"), 404
-
-app.register_error_handler(404, page_not_found)
-
-app.add_url_rule('/favicon.ico', 'redirect_to', redirect_to);
-
-# thread_1 = threading.Thread(target = watch_file, args=());
-# thread_1.start();
 
 if __name__ == '__main__':
    app.run('0.0.0.0', port=5001, debug=True);
